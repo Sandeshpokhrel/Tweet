353ccc23220b73ef3c08bf40945ea28f8e40d3e9
Sandeshpokhrel <yespokhrel9999@gmail.com>
2022-01-30 21:27:56 +0545

    This is pug

--- Tweet.js
+++ Tweet.js
@@ -1,2 +1,9 @@
 const express = require("express");
-const 
+const port =3003;
+const app = express();
+const server = app.listen(port,() =>console.log("server listening to port:"+ port));
+app.set("view engine","pug");
+app.set("views","views");
+app.get("/", (req, res, next) => {
+    res.status(200).render('home.pug');
+})
