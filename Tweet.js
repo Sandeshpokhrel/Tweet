23d11ba7eb16688ad0802727a8172dad32d67d92
Sandeshpokhrel <yespokhrel9999@gmail.com>
2022-02-02 16:55:41 +0545

    middleware and payload

--- Tweet.js
+++ Tweet.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const port =3003;
 const app = express();
+const requireLogin = require('./middleware');
 const server = app.listen(port,() =>console.log("server listening to port:"+ port));
 app.set("view engine","pug");
 app.set("views","views");
-app.get("/", (req, res, next) => {
+app.get("/",middleware.requireLogin, (req, res, next) => {
     var payload = {
-        PageTitle:"Home"
+        pageTitle:"Home",
+        pageBlock:"This is me!"
+
     }
 
     res.status(200).render('home.pug',payload);
