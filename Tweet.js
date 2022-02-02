01d8361fdc547c4457a3046edd5f55c275b6a08e
Sandeshpokhrel <yespokhrel9999@gmail.com>
2022-02-02 17:28:38 +0545

    router

--- Tweet.js
+++ Tweet.js
@@ -5,6 +5,10 @@ const requireLogin = require('./middleware');
 const server = app.listen(port,() =>console.log("server listening to port:"+ port));
 app.set("view engine","pug");
 app.set("views","views");
+
+//Routes(PAGES)
+const loginRoute = require('./Routes/loginRoute');
+app.use("/login", loginRoute);
 app.get("/",middleware.requireLogin, (req, res, next) => {
     var payload = {
         pageTitle:"Home",
