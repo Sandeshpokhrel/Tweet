186163b34ae7282ad90838439141e67587f0ef52
Sandeshpokhrel <yespokhrel9999@gmail.com>
2022-11-09 21:19:34 +0545

    css added with styling

--- Routes/registerRoute.js
+++ Routes/registerRoute.js
@@ -1,14 +1,12 @@
-const { Router } = require("express");
 const express = require("express");
 const app = express();
 const router = express.Router();
-app.set("view engine","pug");
-app.set("views","views");
+app.set("view engine", "pug");
+app.set("views", "views");
 router.get("/", (req, res, next) => {
     var payload = {
         pageTitle: "Login"
     }
-
-    res.status(200).render('login.pug', payload);
+    res.status(200).render("register", payload);
 })
 module.exports = router;
--- Tweet.js
+++ Tweet.js
@@ -2,18 +2,23 @@ const express = require("express");
 const port =3003;
 const app = new express();
 const middleware = require('./middleware');
+const path = require("path");
 // can also define the arrow function outside and call it
 const server = app.listen(port, () => console.log("Server is listening to port: " + port));
 app.set("view engine","pug");
-app.set("views","views");
+app.set("views", "views");
+app.use(express.static(path.join(__dirname, "Public")));
 
 //Routes(PAGES)
 const loginRoute = require('./Routes/loginRoute');
+const registerRoute = require('./Routes/registerRoute');
+
 app.use("/login", loginRoute);
+app.use("/register", registerRoute);
 app.get("/", middleware.requireLogin, (req, res, next) => {
     var payload = {
         pageTitle:"Home",
         pageBlock:"This is me!"
     }
-    res.status(200).render('home.pug',payload);
+    res.status(200).render('home',payload);
 })
